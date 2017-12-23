d30fd4a0a3661370b47e1f13a2ff9ddb0c6d7758
jozefkamensky <jzfkamensky@gmail.com>
2017-12-23 16:13:11 +0100

    init of project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './content/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './content/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeDetailComponent } from './content/recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './content/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './content/recipes/recipe-item/recipe-item.component';
+import { RecipesComponent } from './content/recipes/recipes.component';
+import { ShoppingComponent } from './content/shopping/shopping.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
